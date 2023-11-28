b92e7ef9b818208cd584aa48729d5c7dcd29e477
Johnny3140 <Johnnylopez0314@gmail.com>
2023-11-27 21:15:04 -0500

    all routes added and working, edit and new button created

--- app.js
+++ app.js
@@ -25,40 +25,85 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //INDEX ROUTE
 
-app.get('/', async (req, res) => {
+app.get('/reptiles', async (req, res) => {
   try {
-    const reptiles = await Reptile.find({}, 'Name');  
+    const reptiles = await Reptile.find({}, 'Name');
     res.render('index', { reptiles });
   } catch (err) {
     res.status(500).send(err.message);
   }
 });
-
 //NEW ROUTE
+app.get('/reptiles/new', (req, res) => {
+  res.render('new'); 
+});
 
 
 
 //DELETE ROUTE
-
+app.delete('/reptiles/:id', async (req, res) => {
+  try {
+    await Reptile.findByIdAndDelete(req.params.id);
+    res.redirect('/');
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 
 // UPDATE ROUTE
-
+app.put('/reptiles/:id', async (req, res) => {
+  try {
+    const { Name, Hardiness, Tameability, Habitat, Diet } = req.body;
+    await Reptile.findByIdAndUpdate(req.params.id, { Name, Hardiness, Tameability, Habitat, Diet });
+    res.redirect(`/reptiles/${req.params.id}`);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 
 //CREATE ROUTE 
+app.post('/reptiles', async (req, res) => {
+  try {
+    // Retrieve data from the form submission
+    const { Name, Hardiness, Tameability, Habitat, Diet } = req.body;
 
+    // Create a new reptile in the database
+    const newReptile = new Reptile({ Name, Hardiness, Tameability, Habitat, Diet });
+    await newReptile.save();
+
+    res.redirect('/'); // Redirect to the index route after adding a new reptile
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 
 // EDIT ROUTE 
-
+app.get('/reptiles/:id/edit', async (req, res) => {
+  try {
+    const reptile = await Reptile.findById(req.params.id);
+    res.render('edit', { reptile });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 
 // SHOW ROUTE 
+app.get('/reptiles/:id', async (req, res) => {
+  try {
+    const reptile = await Reptile.findById(req.params.id);
+    res.render('show', { reptile });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 
 
-
+// Listening Port 
 app.listen(PORT, ()=> {
     console.log(`Server is running on ${PORT}`)
 })
